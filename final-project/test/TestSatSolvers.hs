11918304d1c0bb88265254555eb787a96efff379
zchown <91749140+zchown@users.noreply.github.com>
2024-11-02 13:48:29 -0400

    sat tests

--- final-project/test/TestSatSolvers.hs
+++ final-project/test/TestSatSolvers.hs
@@ -16,18 +16,37 @@ runTestSatSolver :: IO ()
 runTestSatSolver =
   hspec $ do
     describe "dpll" $ do
+      it "unsatisfiable problem" $ do
+        let problem =
+              fromJust $
+              S.satProblemFromList @4 [[1, 2, 3], [1, 2, 4], [1], [-1]]
+        dpll
+          problem
+          (S.createVarAssignment (S.createVarList @4) (S.createVarList @4)) `shouldBe`
+          S.Unsatisfiable
       it "solves empty problem" $ do
         let problem = fromJust $ S.satProblemFromList @4 []
         dpll
           problem
           (S.createVarAssignment (S.createVarList @4) (S.createVarList @4)) `shouldBe`
-          Just (S.createVarAssignment (S.createVarList @4) (S.createVarList @4))
+          S.Satisfiable (S.createVarList @4)
       it "solves single clause problem" $ do
         let problem = fromJust $ S.satProblemFromList @4 [[1]]
         dpll
           problem
           (S.createVarAssignment (S.createVarList @4) (S.createVarList @4)) `shouldBe`
-          Just
-            (S.createVarAssignment
-               (S.createVarList @4)
-               (S.varListFromList @4 [True, False, False, False]))
+          S.Satisfiable (S.varListFromList @4 [True, True, True, True])
+      it "solves single clause problem with negative" $ do
+        let problem = fromJust $ S.satProblemFromList @4 [[-1]]
+        dpll
+          problem
+          (S.createVarAssignment (S.createVarList @4) (S.createVarList @4)) `shouldBe`
+          S.Satisfiable (S.varListFromList @4 [False, True, True, True])
+      it "solves simple problem" $ do
+        let problem =
+              fromJust $
+              S.satProblemFromList @4 [[1, 2, 3], [-1, -2, -4], [3, 4]]
+        dpll
+          problem
+          (S.createVarAssignment (S.createVarList @4) (S.createVarList @4)) `shouldBe`
+          S.Satisfiable (S.varListFromList @4 [True, True, True, False])
