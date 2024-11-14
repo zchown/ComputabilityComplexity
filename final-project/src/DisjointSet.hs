c16718130df476716c7140511730de0aa78cbe8f
zchown <91749140+zchown@users.noreply.github.com>
2024-11-14 00:08:26 -0500

    disjoint set and graphs

---
+++ final-project/src/DisjointSet.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module DisjointSet where
+
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.MArray
+import Data.Array.ST
+
+data DisjointSet s = DisjointSet
+  { parent :: STUArray s Int Int
+  , rank :: STUArray s Int Int
+  }
+
+createDisjointSet :: Int -> ST s (DisjointSet s)
+createDisjointSet n = liftM2 DisjointSet (newListArray (1, n) [1 .. n]) (newArray (1, n) 0)
+
+getRepresentative :: DisjointSet s -> Int -> ST s Int
+getRepresentative ds x = do
+  p <- readArray (parent ds) x
+  if p /= x
+    then do
+      rep <- getRepresentative ds p
+      writeArray (parent ds) x rep
+      return rep
+    else return p
+
+find :: DisjointSet s -> Int -> Int -> ST s Bool
+find ds x y = liftM2 (==) (getRepresentative ds x) (getRepresentative ds y)
+
+link :: DisjointSet s -> Int -> Int -> ST s ()
+link ds x y = do
+  rx <- readArray (rank ds) x
+  ry <- readArray (rank ds) y
+  case compare rx ry of
+    GT -> writeArray (parent ds) y x
+    LT -> writeArray (parent ds) x y
+    EQ -> writeArray (rank ds) y (ry + 1)
+
+union :: DisjointSet s -> Int -> Int -> ST s ()
+union ds x y = do
+  rx <- getRepresentative ds x
+  ry <- getRepresentative ds y
+  unless (rx == ry) $ link ds rx ry
---
+++ final-project/src/GraphTypes.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module GraphTypes where
+
+import Control.Monad (forM_)
+import Control.Monad.ST (ST)
+import Data.Array.Base
+import Data.Array.ST
+import Data.Array.Unboxed (UArray)
+
+newtype NodeId =
+  NodeId Int
+  deriving (Show, Eq, Ord, Ix)
+
+newtype EdgeWeight =
+  EdgeWeight Double
+  deriving (Show, Eq, Ord, Num)
+
+-- instance MArray (STUArray s) EdgeWeight ST where
+--     getBounds = getBounds . castSTUArray
+--     getNumElements = getNumElements . castSTUArray
+--     newArray b e = newArray b (realToFrac e) >>= return . castSTUArray
+--     newArray_ b = newArray_ b >>= return . castSTUArray
+--     unsafeRead arr i = unsafeRead (castSTUArray arr) i >>= return . realToFrac
+--     unsafeWrite arr i e = unsafeWrite (castSTUArray arr) i (realToFrac e)
+--
+-- instance IArray UArray EdgeWeight where
+--     bounds = bounds . castUArray
+--     numElements = numElements . castUArray
+--     unsafeArray b es = castUArray $ unsafeArray b [(i, realToFrac e) | (i,e) <- es]
+--     unsafeAt arr i = EdgeWeight $ realToFrac (unsafeAt (castUArray arr) i)
+-- newtype AdjacencyMatrix = AdjacencyMatrix { matrix :: UArray (NodeId, NodeId) EdgeWeight }
+--   deriving (Show, Eq)
+data BasicGraph = BasicGraph
+  { bgNodes :: [NodeId]
+  , bgEdges :: [(NodeId, NodeId, EdgeWeight)]
+  } deriving (Show, Eq)
+
+data Node = Node
+  { nNodeId :: NodeId
+  , nEdges :: [(NodeId, EdgeWeight)]
+  } deriving (Show, Eq)
+
+newtype NodeGraph = NodeGraph
+  { ngNodes :: [Node]
+  } deriving (Show, Eq)
+
+------------------------------
+-- | Conversion functions | --
+------------------------------
+basicToNode :: BasicGraph -> NodeGraph
+basicToNode (BasicGraph ns es) = NodeGraph $ map mkNode ns
+  where
+    mkNode n = Node n (map (\(from, to, w) -> (to, w)) $ connecting n)
+    connecting n = filter (\(from, _, _) -> from == n) es
+
+nodeToBasic :: NodeGraph -> BasicGraph
+nodeToBasic (NodeGraph ns) = BasicGraph (map nNodeId ns) (concatMap mkEdges ns)
+  where
+    mkEdges (Node n es) = map (\(to, w) -> (n, to, w)) es
+--
+-- basicToAdjacency :: BasicGraph -> AdjacencyMatrix
+-- basicToAdjacency bg@(BasicGraph ns es) = AdjacencyMatrix table
+--   where
+--     size = maximum $ map (\(NodeId x) -> x) ns
+--     table :: UArray (NodeId, NodeId) EdgeWeight
+--     table = runSTUArray $ do
+--       arr <- newArray ((NodeId 1, NodeId 1), (NodeId size, NodeId size)) (EdgeWeight 0)
+--       forM_ es $ \(from, to, w) -> writeArray arr (from, to) w
+--       return arr
+--
+-- nodeToAdjacency :: NodeGraph -> AdjacencyMatrix
+-- nodeToAdjacency = basicToAdjacency . nodeToBasic
