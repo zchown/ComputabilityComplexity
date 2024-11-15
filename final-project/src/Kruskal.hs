79ee3f893029e49d3153be1088ecdf227d740925
zchown <91749140+zchown@users.noreply.github.com>
2024-11-15 00:58:00 -0500

    better sorting

--- final-project/src/Kruskal.hs
+++ final-project/src/Kruskal.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Kruskal where
 
@@ -28,8 +29,40 @@ kruskal (BasicGraph nodes edges) =
       []
       sortedEdges
 
+-- this implementation is stolen from Data.List
 sortEdges :: [Edge] -> [Edge]
 sortEdges = mergeAll . runs
+  where
+    runs (x:y:zs)
+      | x `f` y == GT = descending y [x] zs
+      | otherwise = ascending y (x :) zs
+    runs xs = [xs]
+    descending y acc (x:xs)
+      | y `f` x == GT = descending x (y : acc) xs
+    descending y acc xs = (y : acc) : runs xs
+    ascending y acc (x:xs)
+      | y `f` x /= GT = ascending x (\ys -> acc (y : ys)) xs
+    ascending y acc xs =
+      let !x = acc [y]
+       in x : runs xs
+    mergeAll [x] = x
+    mergeAll xs = mergeAll (mergePairs xs)
+    mergePairs (x:y:zs) =
+      let !a = merge x y
+       in a : mergePairs zs
+    mergePairs xs = xs
+    merge xs@(x:xs') ys@(y:ys')
+      | x `f` y == GT = y : merge xs ys'
+      | otherwise = x : merge xs' ys
+    merge [] ys = ys
+    merge xs [] = xs
+    f (Edge (_, _, w1)) (Edge (_, _, w2)) = compare w1 w2
+
+-- this is the best implementation I came up with
+-- it is based on the same ideas as the previous one
+-- but is less optimized and less readable :(
+sortEdges' :: [Edge] -> [Edge]
+sortEdges' = mergeAll . runs
   where
     runs [] = []
     runs [x] = [[x]]
--- final-project/src/TspSolvers.hs
+++ final-project/src/TspSolvers.hs
@@ -1,1 +1,10 @@
 module TspSolvers where
+
+import GraphTypes
+import Kruskal
+
+tspSolver :: BasicGraph -> BasicGraph
+tspSolver = backtrack . kruskal
+  where
+    backtrack (BasicGraph nodes edges) =
+      BasicGraph nodes $ concatMap backtrackEdge edges
