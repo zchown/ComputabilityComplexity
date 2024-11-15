bb2e6924d24772593ed6521bd5f2b46f72878c57
zchown <91749140+zchown@users.noreply.github.com>
2024-11-15 13:09:37 -0500

    tspsolver

--- final-project/src/TspSolvers.hs
+++ final-project/src/TspSolvers.hs
@@ -3,8 +3,14 @@ module TspSolvers where
 import GraphTypes
 import Kruskal
 
-tspSolver :: BasicGraph -> BasicGraph
-tspSolver = backtrack . kruskal
+data TspSolution = TspSolution
+  { tspCost :: Double 
+  , tspPath :: [Edge]
+  } deriving (Show, Eq)
+
+tspSolver :: BasicGraph -> TspSolution
+tspSolver g = TspSolution cost path
   where
-    backtrack (BasicGraph nodes edges) =
-      BasicGraph nodes $ concatMap backtrackEdge edges
+    (BasicGraph _ mstEdges) = kruskal g
+    path = concat $ scanl (flip (:)) [last mstEdges] (init mstEdges)
+    (EdgeWeight cost) = sum $ map (\(Edge (_, _, w)) -> w) path
