52763363c0429f3f3cc230bc204cbd1d02e109df
zchown <91749140+zchown@users.noreply.github.com>
2024-12-09 11:56:50 -0500

    better tsp benchmarking

--- final-project/bench/TspBenchmarks.hs
+++ final-project/bench/TspBenchmarks.hs
@@ -1,32 +1,29 @@
 module TspBenchmarks where
 
 import GraphTypes
-import System.Directory (doesFileExist, listDirectory)
-import System.FilePath ((</>))
+import System.Directory (doesFileExist)
 import TspLibParser
 import TspSolvers
-import TspTypes
 
 runTspBenchmarks :: IO ()
 runTspBenchmarks = do
   putStrLn "Running TSP benchmarks..."
   let problems =
-        [ "burma14.tsp.gz"
-        , "ali535.tsp.gz"
-        , "berlin52.tsp.gz"
-        , "bier127.tsp.gz"
-        , "ch130.tsp.gz"
-        , "ch150.tsp.gz"
-        , "d198.tsp.gz"
-        , "d493.tsp.gz"
-        , "d657.tsp.gz"
-        , "eil51.tsp.gz"
+        [ ("tsp_problems/burma14.tsp.gz", 3323)
+        , ("tsp_problems/ali535.tsp.gz", 202310)
+        , ("tsp_problems/berlin52.tsp.gz", 7542)
+        , ("tsp_problems/bier127.tsp.gz", 118282)
+        , ("tsp_problems/ch130.tsp.gz", 6110)
+        , ("tsp_problems/ch150.tsp.gz", 6528)
+        , ("tsp_problems/d198.tsp.gz", 15780)
+        , ("tsp_problems/d493.tsp.gz", 35002)
+        , ("tsp_problems/d657.tsp.gz", 48912)
+        , ("tsp_problems/eil51.tsp.gz", 426)
         ]
-  let dp = map ("tsp_problems/" ++) problems
-  mapM_ runTsp dp
+  mapM_ runTsp problems
 
-runTsp :: String -> IO ()
-runTsp file = do
+runTsp :: (String, Double) -> IO ()
+runTsp (file, csol) = do
   exists <- doesFileExist file
   if exists
     then do
@@ -39,5 +36,9 @@ runTsp file = do
             Right err -> putStrLn $ "FAILED\n    Error: " ++ show err
             Left b -> do
               let sol = tspSolver b
-              putStrLn $ file ++ ": " ++ (show . tspCost) sol
+              putStrLn $
+                file ++
+                ": " ++
+                (show . tspCost) sol ++
+                " - " ++ show csol ++ " - " ++ show (tspCost sol / csol)
     else putStrLn $ "FAILED\n    Error: File not found: " ++ file
